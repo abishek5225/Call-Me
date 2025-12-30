615bd47cff1ea33b5d55d880ff82f48abadfcd97
Abishek CHaulagain <abishekchaulagain5225@gmail.com>
2025-12-30 10:30:23 +0545

    uploaded

---
+++ app/call/[callId]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+ 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function HomePage(){
+    const [username, setUsername] = useState('abishek');
+    const [callId, setCallId] = useState('test-room');
+    const router = useRouter();
+    
+    
+
+    const joinCall = () => {
+        if(!username || !callId) return;
+        router.push(`/call/${callId}?user=${username}`)
+    }
+
+    return(
+        <main className="p-40">
+            <h1>Join Video Call</h1>
+            <input
+            placeholder="Your name"
+            value= {username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+
+             <input
+        placeholder="Call ID (room name)"
+        value={callId}
+        onChange={(e) => setCallId(e.target.value)}
+      />
+      <br />
+
+      <button onClick={joinCall}>Join</button>
+        </main>
+    )
+}
